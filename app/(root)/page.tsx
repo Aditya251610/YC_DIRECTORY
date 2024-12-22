436a2ee735128448b6b7f34f97d655e3e9b54f8e
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-12-22 15:12:12 +0530

    understood caching in nextjs and implemented the search fetching using query params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,18 @@
 import SearchForm from "../../components/SearchForm";
-import StartupCard from "../../components/StartupCard";
+import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams } : { searchParams: Promise<{ query?: string }> }) {
 
   const query = (await searchParams).query;
 
-  const posts = [{
-    _createdAt: new Date(),
-    views: 55,
-    author: { _id: 1, name: 'Aditya' },
-    _id: 1,
-    description: 'This is a description',
-    image: 'https://cdn.pixabay.com/photo/2023/04/29/07/13/ai-generated-7957936_960_720.jpg',
-    category: 'Robots',
-    title: 'We Robots',
-  }]
+  const params = { search: query || null };
+
+
+  // const posts = await client.fetch(STARTUP_QUERY)
+
+  const { data: posts } = await sanityFetch({query: STARTUP_QUERY, params})
 
   return (
     <>
@@ -37,7 +35,7 @@ export default async function Home({ searchParams } : { searchParams: Promise<{ 
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
@@ -45,6 +43,8 @@ export default async function Home({ searchParams } : { searchParams: Promise<{ 
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
